cafc226b4baa64a7d772cbe6f94f18d37a767406
Jonas Malassa <jonas.malassa@methodpark.de>
2019-05-20 18:49:53 +0200

    Extend PiCamera for subclassing PiRGBAnalysis

--- fake_rpi/picamera.py
+++ fake_rpi/picamera.py
@@ -5,44 +5,68 @@ from .Base import Base
 
 
 class BGR(object):
-	"""Fake class"""
+    """Fake class"""
 
-	@printf
-	def __init__(self, sz):
-		# constructor
-		self.array = np.random.rand(*sz)
+    @printf
+    def __init__(self, sz):
+        # constructor
+        self.array = np.random.rand(*sz)
 
-	@printf
-	def truncate(self, num):
-		# refreshes the fake image
-		self.array = np.random.rand(*self.array.shape)
+    @printf
+    def truncate(self, num):
+        # refreshes the fake image
+        self.array = np.random.rand(*self.array.shape)
 
 
 # class picamera(object):
 # 	"""Fake class"""
 class PiCamera(Base):
-	"""Fake class"""
-	resolution = (0, 0)
+    """Fake class"""
+    resolution = (0, 0)
 
-	def __init__(self):
-		# empty constructor
-		# print('WARNING: Fake_RPi PiCamera on {}'.format(platform.system().lower()))
-		Base.__init__(self, self.__class__)
-		pass
+    def __init__(self, sensor_mode, resolution, framerate):
+        self.sensor_mode = sensor_mode
+        self.resolution = resolution
+        self.framerate = framerate
+        # empty constructor
+        # print('WARNING: Fake_RPi PiCamera on {}'.format(platform.system().lower()))
+        Base.__init__(self, self.__class__)
+        pass
 
-	def close(self):
-		# this does nothing
-		pass
+    def close(self):
+        # this does nothing
+        pass
 
-	@printf
-	def capture(self, image, format, use_video_port):
-		# this does nothing
-		pass
+    @printf
+    def capture(self, image, format, use_video_port):
+        # this does nothing
+        pass
+
+    @printf
+    def start_recording(self, output, format=None, resize=None, splitter_port=1, **options):
+        pass
+
+    def wait_recording(self, timeout=0, splitter_port=1):
+        pass
+
+    @printf
+    def stop_recording(self, splitter_port=1):
+        pass
+
+
+class PiRGBAnalysis:
+    """"Fake class"""
+
+    def __init__(self, cam, size=None):
+        pass
+
+    def write(self, b):
+        pass
 
 
 class array(object):
-	"""Fake class"""
+    """Fake class"""
 
-	@staticmethod
-	def PiRGBArray(cam, size):
-		return BGR(size)
+    @staticmethod
+    def PiRGBArray(cam, size):
+        return BGR(size)
